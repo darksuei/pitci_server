13b77b924d8ca63931e1762b8996b6354787f1b5
darksuei <chrisraphael576@gmail.com>
2024-06-24 11:34:08 +0100

    Feat: Update environment setup

--- src/config/readEnv.config.ts
+++ src/config/readEnv.config.ts
@@ -2,22 +2,10 @@ import dotenv from "dotenv";
 import path from "path";
 
 export function configureEnvironment(): void {
-  if (process.env.NODE_ENV === "production") {
-    dotenv.config({
-      path: path.resolve(process.cwd(), ".env.production"),
-      override: true,
-    });
-  } else if (process.env.NODE_ENV === "test") {
-    dotenv.config({
-      path: path.resolve(process.cwd(), ".env.test"),
-      override: true,
-    });
-  } else {
-    dotenv.config({
-      path: path.resolve(process.cwd(), ".env"),
-      override: true,
-    });
-  }
+  dotenv.config({
+    path: path.resolve(process.cwd(), ".env"),
+    override: true,
+  });
 }
 configureEnvironment();
 
