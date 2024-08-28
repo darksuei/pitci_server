1544825100fc9f0702f8f1cf2e3a6fe06722441a
darksuei <chrisraphael576@gmail.com>
2024-08-28 19:02:52 +0100

    Add authentication to internal routes

--- src/routes/internalRoutes.ts
+++ src/routes/internalRoutes.ts
@@ -2,9 +2,10 @@ import express from "express";
 import { postCreateSuperAdmin } from "../controllers/internalControllers/postCreateSuperAdmin";
 import { authorization } from "../middlewares/authorization";
 import { RoleEnum } from "../utils/enums";
+import { authenticate } from "../middlewares/authenticate";
 
 const router = express.Router();
 
-router.post("/create-super-admin", authorization(RoleEnum.SUPER_ADMIN), postCreateSuperAdmin);
+router.post("/create-super-admin", authenticate, authorization(RoleEnum.SUPER_ADMIN), postCreateSuperAdmin);
 
 export default router;
